4396e0ff523077f1682a35c3f25d249be725b316
spustoff <vyacheslavpustov05@gmail.com>
2023-11-15 13:45:20 +0300

    Initial Commit

---
+++ App256/App256App.swift
@@ -0,0 +1,17 @@
+//
+//  App256App.swift
+//  App256
+//
+//  Created by Вячеслав on 11/15/23.
+//
+
+import SwiftUI
+
+@main
+struct App256App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App256/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  App256
+//
+//  Created by Вячеслав on 11/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
